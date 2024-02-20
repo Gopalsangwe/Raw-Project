e702af000e27e3458e08e938b9dc40744bdf3bfc
Gopal Sangwe <gopalsangwe15@gmail.com>
2024-02-21 00:13:11 +0530

    backend updated

--- backEnd/car_rental_system/src/main/java/com/app/dao/CarDAO.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Car;
-
-public interface CarDAO extends JpaRepository<Car, Long> {
-
-}
